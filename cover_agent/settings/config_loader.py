d58b637c28e16499eeedd972dff3b7a4d6d01599
Embedded DevOps <info@embeddeddevops.com>
2024-05-23 16:57:07 -0700

    Add file existence check.

--- cover_agent/settings/config_loader.py
+++ cover_agent/settings/config_loader.py
@@ -1,8 +1,9 @@
-from os.path import dirname, abspath, join
+from os.path import dirname, abspath, join, exists
 
 from dynaconf import Dynaconf
 
 current_dir = dirname(abspath(__file__))
+SETTINGS_FILES = ["test_generation_prompt.toml", "language_extensions.toml", ]
 
 class SingletonSettings:
     _instance = None
@@ -13,14 +14,35 @@ class SingletonSettings:
         return cls._instance
 
     def __init__(self):
+        """
+        Initialize the SingletonSettings instance by loading settings from the specified configuration files.
+        Check if the 'settings' attribute is not already set, then construct the paths to the settings files based on the current directory.
+        Ensure that all settings files exist by checking their presence using the 'os.path.exists' function.
+        If any of the settings files are missing, raise a 'FileNotFoundError' with a message indicating the missing file.
+        Finally, initialize the 'settings' attribute with a Dynaconf instance using the specified settings files and configuration options.
+
+        Parameters:
+            self: SingletonSettings
+                The SingletonSettings instance to be initialized.
+
+        Raises:
+            FileNotFoundError: If any of the specified settings files are not found.
+
+        Returns:
+            None
+        """
         if not hasattr(self, "settings"):
             current_dir = dirname(abspath(__file__))
+            settings_files = [join(current_dir, f) for f in SETTINGS_FILES]
+            # Ensure all settings files exist
+            for file in settings_files:
+                if not exists(file):
+                    raise FileNotFoundError(f"Settings file not found: {file}")
+            
             self.settings = Dynaconf(
                 envvar_prefix=False,
                 merge_enabled=True,
-                settings_files=[join(current_dir, f) for f in [
-                    "test_generation_prompt.toml",
-                    "language_extensions.toml", ]]
+                settings_files=settings_files
             )
 
 
