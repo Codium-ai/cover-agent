a4ac386821c455606ef7cb5311e98a217081eb96
Embedded DevOps <info@embeddeddevops.com>
2024-09-06 11:30:21 -0700

    Fixed arg call for validate test.

--- cover_agent/CoverAgent.py
+++ cover_agent/CoverAgent.py
@@ -77,7 +77,7 @@ class CoverAgent:
 
             for generated_test in generated_tests_dict.get("new_tests", []):
                 test_result = self.test_gen.validate_test(
-                    generated_test, generated_tests_dict, self.args.run_tests_multiple_times
+                    generated_test, self.args.run_tests_multiple_times
                 )
                 test_results_list.append(test_result)
 
