ff22b917ac2b941dfcc29898593bfe3e769acf65
mrT23 <tal.r@codium.ai>
2024-11-13 13:45:39 +0200

    Add compatibility for Python versions before 3.11 by using typing_extensions

--- cover_agent/lsp_logic/multilspy/multilspy_types.py
+++ cover_agent/lsp_logic/multilspy/multilspy_types.py
@@ -5,7 +5,10 @@ Defines wrapper objects around the types returned by LSP to ensure decoupling be
 from __future__ import annotations
 
 from enum import IntEnum, Enum
-from typing import NotRequired, TypedDict, List, Dict, Union
+try:
+    from typing import NotRequired, TypedDict, List, Dict, Union
+except ImportError: # before Python 3.11
+    from typing_extensions import NotRequired, TypedDict, List, Dict, Union
 
 URI = str
 DocumentUri = str
